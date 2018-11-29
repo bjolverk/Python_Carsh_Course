2740285d90db4610f0fe7d33f6145138ffb8d5b2
Anton <bjolverk@gmail.com>
2018-11-29 12:03:29 +0300

    End of 2-th lesson

---
+++ Lesson_2/Homework/name_cases.py
@@ -0,0 +1,13 @@
+user_name = "Anton"
+print("Hello " + user_name + ", would you like to learn some Python today?")
+famous_person = "Никола Тесла"
+cite = "говорил:\"Вам знакомо выражение:\n\"Выше головы не прыгнешь\"?\nЭто- заблуждение. Человек может всё.\""
+message = famous_person + " " + cite
+print(message)
+new_user_name = input("Введите Ваше имя: ")
+print(new_user_name)
+new_user_name = new_user_name.strip()
+print(new_user_name)
+print(new_user_name.upper())
+print(new_user_name.lower())
+print(new_user_name.title())
